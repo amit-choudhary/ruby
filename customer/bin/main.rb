e24df24a7eec2cf16f6fb72cd5167301e0e8bd4c
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-08 12:19:55 +0530

    Fix functioning and indentation

--- customer/bin/main.rb
+++ customer/bin/main.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/customer.rb'
 
 first_customer = Customer.new("Amit")
-p first_customer
+puts first_customer
 puts "Depositing Rs.100"
 first_customer.deposit(100)
-puts "Balance : #{first_customer.balance}"
+puts "Balance : #{ first_customer.balance }"
 puts "Withdrawing Rs.200"
 first_customer.withdraw(1200)
-puts "Balance : #{first_customer.balance}"
+puts "Balance : #{ first_customer.balance }"
+second_customer = Customer.new("Nitin", 2000)
+puts second_customer
--- customer/lib/customer.rb
+++ customer/lib/customer.rb
@@ -1,12 +1,13 @@
 class Customer
+  @@account_number = 0
+  attr_accessor :balance
+  attr_reader :account_no
 
-  @@account_no = 0
-  attr_accessor :balance, :account_no
-
-  def initialize(name)
+  def initialize(name, balance = 1000)
     @name = name
-    @account_no = @@account_no + 1 
-    @balance = 1000
+    @account_no = @@account_number + 1
+    @@account_number += 1
+    @balance = balance
   end
 
   def deposit(amount)
@@ -19,6 +20,10 @@ class Customer
     else
       @balance -= amount
     end
+  end
+
+  def to_s
+    "Name: #{ @name } Account No.: #{ @account_no } Balance: #{ @balance }"
   end
 
 end
