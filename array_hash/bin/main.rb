e7489d40e00947be595cd7c8cc29a7f2462e1cdf
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-08 13:24:05 +0530

    Array Hash Exercise

---
+++ array_hash/bin/main.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/array.rb'
+
+puts 'Enter the size of the array: '
+size = (gets.chomp.to_i - 1)
+puts 'Enter the array elements: '
+input_array = Array.new(size)
+
+for i in (0..size)
+  input_array[i] = gets.chomp
+end
+
+print input_array
+print "\n"
+print input_array.create_hash
---
+++ array_hash/lib/array.rb
@@ -0,0 +1,13 @@
+class Array
+  def create_hash
+    array_hash = Hash.new { |hash, key| hash[key]=[] }
+
+    for i in 0...size
+      array_hash[self[i].to_s.length] << self[i]
+    end
+
+    array_hash
+
+  end
+
+end
