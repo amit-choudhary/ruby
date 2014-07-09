8ca1a9a2bf4c0f5a24ef0534b6e15fd524335104
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-09 18:10:02 +0530

    Add Custom Exception

--- factorial_exception/bin/main.rb
+++ factorial_exception/bin/main.rb
@@ -1,15 +1,16 @@
 require_relative '../lib/fixnum.rb'
+require_relative '../lib/badargument.rb'
 
 begin
 print 'Enter the input number: '
 input_num = gets.chomp.to_i
 if input_num < 0
-  raise ArgumentError
+  raise BadArgument
 else
 puts input_num.factorial
 end
 
-rescue ArgumentError
+rescue BadArgument
   puts "Bad Argument"
   retry
 end
