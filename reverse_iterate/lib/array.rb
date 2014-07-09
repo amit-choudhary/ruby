1a80ce1c91ae8525884d95f61096c1fda4bf80ac
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-09 17:26:36 +0530

    Fix functioning

--- reverse_iterate/lib/array.rb
+++ reverse_iterate/lib/array.rb
@@ -1,9 +1,11 @@
 class Array
 
   def reverse_iterate
-    str = join(' ')
-    str = str.reverse
-    yield str
+    length = size - 1
+    while length > -1
+    yield self[length]
+    length -= 1 
+    end
   end
 
 end
