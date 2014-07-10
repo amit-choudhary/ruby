d9a5361a0ba4abf06565a30ab1584b7de2e64cd4
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-10 13:03:48 +0530

    Fix indentation and variable name

--- reverse_iterate/lib/array.rb
+++ reverse_iterate/lib/array.rb
@@ -1,10 +1,10 @@
 class Array
 
   def reverse_iterate
-    length = size - 1
-    while length > -1
-    yield self[length]
-    length -= 1 
+    max_index_of_array = size - 1
+    while max_index_of_array > -1
+      yield self[max_index_of_array]
+      length_of_array -= 1 
     end
   end
 
