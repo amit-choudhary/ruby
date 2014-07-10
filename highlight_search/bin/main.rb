22a60edc6fcf07ebbb3b2d92b8f46a2afa8f4369
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-10 11:20:55 +0530

    optimise code

--- highlight_search/bin/main.rb
+++ highlight_search/bin/main.rb
@@ -2,4 +2,6 @@ require_relative '../lib/string.rb'
 
 print 'Enter the input string: '
 input_str = gets.chomp
-puts input_str.highlight_search('can')
+print 'Enter the text to be highlighted: '
+highlight_text = gets.chomp
+puts input_str.highlight_search(highlight_text)
