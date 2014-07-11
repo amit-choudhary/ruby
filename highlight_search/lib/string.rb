911167149acdec07d214c48363270e49cbc5c674
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-11 17:43:35 +0530

    Optimise code

--- highlight_search/lib/string.rb
+++ highlight_search/lib/string.rb
@@ -1,8 +1,13 @@
 class String
 
   def highlight_search(str)
+  	occurence_count = 0
     regex_word = /(#{str})/i
-    "#{ self.gsub(regex_word, '(\1)') } \nTotal occurences found: #{ self.scan(regex_word).length }"
+    new_str = gsub(regex_word) do |match|
+      occurence_count += 1
+       "(#{ match })"
+    end
+    "#{ new_str } \nTotal occurences found: #{ occurence_count }"
   end
 
 end
