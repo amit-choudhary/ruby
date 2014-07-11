da73a975ba9dec59765191e76d4a9a294d8d854e
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-11 14:51:28 +0530

    Fix exception issue

--- reverse_iterate/lib/array.rb
+++ reverse_iterate/lib/array.rb
@@ -1,10 +1,10 @@
 class Array
 
   def reverse_iterate
-    max_index_of_array = size - 1
-    while max_index_of_array > -1
-      yield self[max_index_of_array]
-      length_of_array -= 1 
+    index_of_array = size - 1
+    while index_of_array > -1
+      yield self[index_of_array]
+      index_of_array -= 1 
     end
   end
 
