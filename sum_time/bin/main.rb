1dc49014e2ae0258c3514f56ca654c55f5efd786
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-12 12:25:26 +0530

    Optimise code

--- sum_time/bin/main.rb
+++ sum_time/bin/main.rb
@@ -1,8 +1,9 @@
-require_relative '../lib/time.rb'
+require_relative '../lib/time'
 require 'date'
 
 print 'Enter First time: '
 first_time = gets.chomp
 print 'Enter Second_time: '
 second_time = gets.chomp
-print Time.sum(first_time, second_time)
+two_times = Sum.new
+print two_times.sum(first_time, second_time)
--- sum_time/lib/time.rb
+++ sum_time/lib/time.rb
@@ -1,23 +1,21 @@
-class Time
+require 'time'
 
-  REGEX = /^([0-1]\d|[2][0-3])[:][0-5]\d[:][0-5]\d$/
+class Sum
 
-  def self.valid?(str)
+  REGEX = /^([0-1]?\d?|[2]?[0-3]?)[:][0-5]?\d?[:][0-5]?\d?$/
+
+  def valid?(str)
     REGEX =~ str
   end
 
-  def self.string_to_time(time_string)
-    DateTime.parse(time_string + 'IST').to_time
-  end
-
-  def self.sum(first_time = '00:00:00', second_time = '00:00:00')
+  def sum(first_time = '00:00:00', second_time = '00:00:00')
     if valid?(first_time) && valid?(second_time)
-      time1, time2 = string_to_time(first_time), string_to_time(second_time)
+      time1, time2 = Time.parse(first_time), Time.parse(second_time)
       total_time = time1 + time2.sec + time2.min * 60 + time2.hour * 3600
-      total_string = "#{ final_time.hour }:" + "#{ final_time.min }:" + "#{ final_time.sec }"
+      total_string = "#{ total_time.hour }:" + "#{ total_time.min }:" + "#{ total_time.sec }"
       ((time1.hour + time2.hour) > 23 ? "1 day & " : "") + total_string
     else
-      'Invalid'
+      'Invalid time.'
     end
   end
 
