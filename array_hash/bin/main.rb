d5efb6d0df0459da1128182a44a93a62d744071b
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-14 10:46:25 +0530

    Optimise code

--- array_hash/bin/main.rb
+++ array_hash/bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/array.rb'
 puts 'Enter the size of the array: '
 size = gets.chomp.to_i
 puts 'Enter the array elements: '
-input_array = Array.new()
+input_array = Array.new
 
 size.times do |index|
   input_array.push(gets.chomp)
