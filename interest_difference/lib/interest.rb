ca3cb91cd6fdfea45889934c224c649b3a36ad1b
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-14 11:32:19 +0530

    Add guard for block initialization and use of attr_accessor instead of instance variables

--- interest_difference/lib/interest.rb
+++ interest_difference/lib/interest.rb
@@ -2,19 +2,20 @@ class Interest
   attr_accessor :principal, :time, :rate
 
   def initialize
-    yield self
+    rate = 10
+    yield self if block_given?
   end
   
   def simple_amount
-    @principal + @principal * @time * @rate / 100
+    principal + principal * time * @rate / 100
   end
 
-  def compund_amount
-    @principal * ((1 + @rate / 100.to_f) ** @time)
+  def compound_amount
+    principal * ((1 + rate / 100.to_f) ** time)
   end
 
   def diff_in_amount
-    compund_amount - simple_amount
+    compound_amount - simple_amount
   end
 
 end
