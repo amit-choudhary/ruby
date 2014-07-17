e49d0d9c4b4dca6b15de2c42a182f01fd050dc98
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-17 19:10:33 +0530

    Remove use of instance variables

--- interest_difference/lib/interest.rb
+++ interest_difference/lib/interest.rb
@@ -2,8 +2,8 @@ class Interest
   attr_accessor :principal, :time, :rate
 
   def initialize
-    rate = 10
     yield self if block_given?
+    raise ArgumentError if principal.nil? || time.nil? || rate.nil?
   end
   
   def simple_amount
