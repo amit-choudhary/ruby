f44b8c3a7827becb63580c39b38074a2cdabb995
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-18 10:36:17 +0530

    Fix functioning and remove use of instance and seperate functionalities

--- sum_time/bin/main.rb
+++ sum_time/bin/main.rb
@@ -2,11 +2,11 @@ require_relative '../lib/time'
 require 'date'
 
 begin
-print 'Enter First time: '
-first_time = gets.chomp
-print 'Enter Second_time: '
-second_time = gets.chomp
-print Time.show_total_time(first_time, second_time)
+  print 'Enter First time: '
+  first_time = gets.chomp
+  print 'Enter Second_time: '
+  second_time = gets.chomp
+  print Time.show_total_time(first_time, second_time)
 rescue InvalidTimeError
   puts 'Invalid Time. Please Enter again.'
   retry
--- sum_time/lib/time.rb
+++ sum_time/lib/time.rb
@@ -3,25 +3,22 @@ require_relative '../lib/invalid_time_error'
 
 class Time
   REGEX = /^([0-1]?\d?|[2]?[0-3]?)[:][0-5]?\d?[:][0-5]?\d?$/
-  @@total_string = ''
-  @@total_time = Time.parse('00:00:00')
-  @@day = 0
 
   def self.valid?(str)
     REGEX =~ str
   end
 
-  def self.sum(first_time, second_time)
-    time1, time2 = Time.parse(first_time), Time.parse(second_time)
-    @@total_time = time1 + time2.sec + time2.min * 60 + time2.hour * 3600
-    @@day = @@total_time.day - time1.day
+  def self.sum(time1, time2)
+    total_time = time1 + time2.sec + time2.min * 60 + time2.hour * 3600
   end
 
   def self.show_total_time(first_time = '00:00:00', second_time = '00:00:00')
     if valid?(first_time) && valid?(second_time)
-      sum(first_time, second_time)
-      @@total_string = "#{ @@total_time.hour }:" + "#{ @@total_time.min }:" + "#{ @@total_time.sec }"
-      (@@day > 0 ? "1 day & " : "") + @@total_string
+      time1, time2 = Time.parse(first_time), Time.parse(second_time)
+      total_time = sum(time1, time2)
+      total_string = "#{ total_time.hour }:" + "#{ total_time.min }:" + "#{ total_time.sec }"
+      day = total_time.day - time1.day
+      (day > 0 ? "1 day & " : "") + total_string
     else
       raise InvalidTimeError
     end
