2864a51eb5424d82c78a037a0e01d2cdd45c9b30
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-22 18:49:35 +0530

    change argument name from time to times and pluralize day for day > 1

--- sum_time_multiple/lib/time.rb
+++ sum_time_multiple/lib/time.rb
@@ -8,19 +8,25 @@ class Time
     REGEX =~ str
   end
 
-  def self.sum(time)
-    time.inject do |sum, element|
+  def self.sum(times)
+    times.inject do |sum, element|
       sum + element.sec + element.min * 60 + element.hour * 3600
     end
   end
 
-  def self.show_total_time(*time)
-    if time.all? { |time| valid?(time) }
-      time = time.map { |time| Time.parse(time)}
-      total_time = sum(time)
+  def self.show_total_time(*times)
+    if times.all? { |time| valid?(time) }
+      times = times.map { |time| Time.parse(time)}
+      total_time = sum(times)
       total_string = "#{ total_time.hour }:" + "#{ total_time.min }:" + "#{ total_time.sec }"
-      days = total_time.day - time[0].day
-      (days > 0 ? "#{ days } day & " : "") + total_string
+      days = total_time.day - times[0].day
+      if days == 1
+        return "#{ days } day & " + total_string
+      elsif days > 1
+        return "#{ days } days & " + total_string
+      else
+        return total_string
+      end
     else
       raise InvalidTimeError
     end
